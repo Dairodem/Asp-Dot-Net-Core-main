b163aff4bb7b3f0a03ab16074dcc89d9f6ad77d9
demeulemeester dairo <dairo_demeulemeester@hotmail.com>
2020-11-24 10:57:24 +0100

    ErrorHandling

--- MovieWeb/Controllers/HomeController.cs
+++ MovieWeb/Controllers/HomeController.cs
@@ -24,6 +24,11 @@ namespace MovieWeb.Controllers
         {
             return View();
         }
+        [Route("/Oeps")]
+        public IActionResult Oeps()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- MovieWeb/Startup.cs
+++ MovieWeb/Startup.cs
@@ -39,10 +39,11 @@ namespace MovieWeb
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Oeps");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
